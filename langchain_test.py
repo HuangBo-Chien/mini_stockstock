12a755f9c7bc8b8c614863a1dd7537704d0e2bad
HuangBo-Chien <abc29298727@gmail.com>
2025-08-24 13:57:57 +0800

    langchain for fun

--- langchain_test.py
+++ langchain_test.py
@@ -1,9 +1,30 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import os
 
-# 載入 .env 檔案
 load_dotenv()
 
-# 讀取 API 金鑰
-api_key = os.getenv("OPENAI_API_KEY")
-print(api_key)
+model = OllamaLLM(
+    model = "gpt-oss:20b",
+    base_url = os.getenv("OLLAMA_LOCAL_HOST")
+    )
+
+private_placement_template = ChatPromptTemplate.from_template(
+    """
+    請你幫我分析以下的資料表中，哪些是跟募資(private placement)或是面額變更(Stock Par Value Change)有關，並且將有關的index給列出來，如果沒有找到相關的index，請回答-1，謝謝!
+
+    資料表:
+    {df}
+
+    """
+)
+
+if __name__ == "__main__":
+    import pandas as pd
+    info_df = pd.read_csv("test.csv")
+
+    chain = private_placement_template | model
+    res = chain.invoke(input = {"df": info_df.to_string()})
+
+    print(res)
